9f70a0861b55fc871691f153a89bc3a1450f9585
dejian.zhang <iamzhangdejian@outlook.com>
2026-04-16 14:40:47 +0800

    feat: 调整 embedded 模式容器尺寸为 250x250px

--- src/components/Robot/Robot.ts
+++ src/components/Robot/Robot.ts
@@ -82,7 +82,7 @@ export class Robot {
 
     this.robotView = new RobotView(
       wrapper.querySelector('.ai-robot-canvas-container')!,
-      { theme: this.options.theme, size: this.options.embedded ? 280 : 120 }
+      { theme: this.options.theme, size: this.options.embedded ? 250 : 120 }
     );
 
     console.log('[Robot] RobotView created');
@@ -144,8 +144,8 @@ export class Robot {
 
       .ai-robot-wrapper.embedded {
         position: fixed;
-        width: 280px;
-        height: 280px;
+        width: 250px;
+        height: 250px;
         cursor: pointer;
         left: 50%;
         top: 50%;
